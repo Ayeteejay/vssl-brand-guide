f0fdc49ef47fd474bfeee5b454035cae5471b417
Adam Jones <adam@vsslagency.com>
2023-12-13 17:25:49 -0800

    Adding in animation to header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,28 @@
+"use client";
+import { motion } from "framer-motion";
 import Image from "next/image";
 export default function Header() {
   return (
     <nav className="px-10 lg:px-20 absolute top-0 left-0 z-10">
-      <Image
-        src="/images/vssl-logo.svg"
-        alt="VSSL logo"
-        width={500}
-        height={500}
-        priority={true}
-        className="w-40 -translate-y-3 transition-all duration-300 md:w-[250px] md:-translate-y-6"
-      />
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: -100,
+        }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: false }}
+        transition={{ duration: 1.25, type: "tween" }}
+        className="min-w-[300px]"
+      >
+        <Image
+          src="/images/vssl-logo.svg"
+          alt="VSSL logo"
+          width={500}
+          height={500}
+          priority={true}
+          className="w-40 -translate-y-3 transition-all duration-300 md:w-[250px] md:-translate-y-6"
+        />
+      </motion.div>
     </nav>
   );
 }
